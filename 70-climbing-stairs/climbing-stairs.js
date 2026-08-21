0bfb9d326b8f7fc00e59dcc2c5ad8108d1e54e77
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-21 18:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (93.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -4,8 +4,9 @@
  */
 var climbStairs = function (n) {
     let dp = [0, 1, 2];
+
     for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        dp[i] = dp[i - 1] + dp[i - 2]
     }
     return dp[n]
 
