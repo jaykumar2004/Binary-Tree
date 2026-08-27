f046b5db32e5906982978b3fc3d770f15aecc1fa
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-27 17:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (75.48%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (val) {
+    let n = val.length;
+    if (n == 1) return val[0];
+
+    var robHelper = function (start, end) {
+        let p1 = p2 = 0;
+        for (let i = start; i <= end; i++) {
+            curr = Math.max(val[i] + p2, p1)
+            let temp = p1;
+            p1 = curr;
+            p2 = temp;
+            curr++;
+        }
+        return p1;
+    };
+
+    return Math.max(robHelper(0, n - 2), robHelper(1, n - 1))
+};
