facd1b789d1b9194675454518473764a52c9e15a
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-27 13:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (17.51%) - LeetSync

---
+++ 198-house-robber/house-robber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (val) {
+    let n = val.length;
+    if (n == 1) return val[0]
+    let dp = [val[0], Math.max(val[0], val[1])]
+
+    for (let i = 2; i < n; i++) {
+        dp[i] = Math.max(dp[i - 2] + val[i], dp[i - 1])
+    }
+
+    return dp[n-1];
+
+};
