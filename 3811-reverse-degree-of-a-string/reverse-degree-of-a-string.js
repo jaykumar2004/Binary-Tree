9df91ab387ec9e20ced5f4498222f6c54226abf4
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-09-22 16:08:26 +0530

    Time: 2 ms (30.17%) | Memory: 57.8 MB (8.01%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var reverseDegree = function(s) {
+    let sum = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        sum += (('z'.charCodeAt(0) - s.charCodeAt(i)) + 1) * (i + 1);
+    }
+
+    return sum;
+};
